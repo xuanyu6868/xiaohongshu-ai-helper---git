af51973edb24bba287dcedaa6791ae6d17242da5
xuanyu6868 <2284646203@qq.com>
2025-07-02 10:14:11 +0800

    更新

---
+++ src/test/java/com/example/xiaohongshu_ai_helper/XiaohongshuAiHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xiaohongshu_ai_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohongshuAiHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
